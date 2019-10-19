03ece6892ddc3d9e8424d9246fa899aa4dc6636b
parassibal <48132165+parassibal@users.noreply.github.com>
2019-10-20 02:32:33 +0530

    Add files via upload

---
+++ mysignup.js
@@ -0,0 +1,50 @@
+function num()
+{   var haveErr = '';
+    var inputName = document.getElementById("firstname").value;
+    var inputName1 = document.getElementById("lastname").value;
+    var validName = /^[abc]/;
+    if(validName.test(inputName)==false){
+   		haveErr = 'Y';
+	    alert("please enter valid name ");
+    }
+    if(validName.test(inputName1)==false){
+        haveErr = 'Y';
+     alert("please enter valid name ");
+    }
+
+    var inputName2 = document.getElementById("email").value;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(mailformat.test(inputName2)==false){
+        haveErr = 'Y';
+     alert("please enter valid email ");
+    }
+
+
+	var inputtxt=document.getElementById("mobile").value;
+	var phoneno = /^\d{10}$/;
+	if(phoneno.test(inputtxt)==false){
+		haveErr = 'Y';
+	    alert("please enter valid number");
+	}
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("conpassword").value;
+    //var passw=  /^[A-Za-z]\w{7,15}$/;
+    if(password != confirmPassword) {
+    	haveErr = 'Y';
+        alert("Passwords do not match.");       
+    }
+    if (haveErr == 'Y') {
+    	alert("Passwords do not match."); 
+    }
+    else{
+         alert("Successfully submitted");	
+    }   
+}
+function cancelFunction(){
+    var removeName = document.getElementById("firstname").value=" ";
+    var removeName = document.getElementById("lastname").value=" ";
+	var removeMail = document.getElementById("email").value=" ";
+	var removeNumber = document.getElementById("mobile").value=" ";
+	var removePassword = document.getElementById("password").value="";
+	var removeConfirmPassword = document.getElementById("conpassword").value="";
+}
